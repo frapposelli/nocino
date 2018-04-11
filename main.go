7a39081da5bb2a66daf53e7fbfb6443083a655e1
Fabio Rapposelli <fabio@vmware.com>
2018-04-11 18:11:15 +0200

    kick off goroutine for every tg update

--- main.go
+++ main.go
@@ -67,46 +67,46 @@ func main() {
 		for tick := range ticker.C {
 			if err := markov.WriteState(state); err != nil {
 				log.Printf("[state] checkpoint failed: %s (in %s)", err.Error(), time.Since(tick).String())
-			} else {
-				log.Printf("[state] checkpoint completed, %d suffixes in chain (in %s)", len(markov.Chain), time.Since(tick).String())
+				return
 			}
+			log.Printf("[state] checkpoint completed, %d suffixes in chain (in %s)", len(markov.Chain), time.Since(tick).String())
 		}
 	}()
 
 	// feedback loop
 	for update := range updates {
-		answerRequired = false
-		if update.Message == nil || update.Message.Text == "" {
-			continue
-		}
-
-		// tokenize message
-		tokens := strings.Split(update.Message.Text, " ")
-
-		// if it's a reply, check if it's to us, answer back if necessary.
-		if update.Message.ReplyToMessage != nil && update.Message.ReplyToMessage.From.UserName == bot.Self.UserName {
-			log.Printf("[%s] Replied to us, asking: '%s'", update.Message.From.UserName, update.Message.Text)
-			answerRequired = true
-		}
-
-		// check if we're being mentioned, answer back if necessary.
-		if strings.ToLower(tokens[0]) == strings.ToLower(botUsername) {
-			log.Printf("[%s] Mentioned us, asking: '%s'", update.Message.From.UserName, update.Message.Text)
-			// pop the first element
-			tokens = tokens[1:]
-			answerRequired = true
-		}
-
-		if answerRequired {
-			genText, elapsed = markov.GenerateChain(numw, update.Message.Text)
-			log.Printf("[%s] Sending response: '%s' (generated in %s)", update.Message.From.UserName, genText, elapsed.String())
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, genText)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-		}
-
-		// add message to chain
 		go func() {
+			answerRequired = false
+			if update.Message == nil || update.Message.Text == "" {
+				return
+			}
+
+			// tokenize message
+			tokens := strings.Split(update.Message.Text, " ")
+
+			// if it's a reply, check if it's to us, answer back if necessary.
+			if update.Message.ReplyToMessage != nil && update.Message.ReplyToMessage.From.UserName == bot.Self.UserName {
+				log.Printf("[%s] Replied to us, asking: '%s'", update.Message.From.UserName, update.Message.Text)
+				answerRequired = true
+			}
+
+			// check if we're being mentioned, answer back if necessary.
+			if strings.ToLower(tokens[0]) == strings.ToLower(botUsername) {
+				log.Printf("[%s] Mentioned us, asking: '%s'", update.Message.From.UserName, update.Message.Text)
+				// pop the first element
+				tokens = tokens[1:]
+				answerRequired = true
+			}
+
+			if answerRequired {
+				genText, elapsed = markov.GenerateChain(numw, update.Message.Text)
+				log.Printf("[%s] Sending response: '%s' (generated in %s)", update.Message.From.UserName, genText, elapsed.String())
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, genText)
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}
+
+			// add message to chain
 			markov.AddChain(strings.Join(tokens, " "))
 		}()
 	}
