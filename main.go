c4d9f86047bca5b8e1658700f7d1ffd839a0f015
Fabio Rapposelli <fabio@vmware.com>
2018-04-27 17:43:12 +0200

    add gif support

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -22,19 +23,25 @@ var (
 	botUsername    string
 	genText        string
 	trustedIDs     string
+	gifstore       string
+	gifmaxsize     int
 	answerRequired bool
 	elapsed        time.Duration
 	checkpoint     time.Duration
 	markov         *Chain
+	gifdb          *GIFDB
 )
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
+	exe, _ := os.Executable()
 
 	flag.IntVar(&numw, "numw", 25, "maximum number of words for the markov chain")
 	flag.IntVar(&plen, "plen", 2, "chain prefix length")
 	flag.StringVar(&state, "state", "nocino.state.gz", "state file for nocino")
 	flag.StringVar(&tgtoken, "token", "", "telegram bot token")
+	flag.StringVar(&gifstore, "gifstore", fmt.Sprintf("%s/gifs", filepath.Dir(exe)), "path to store GIFs")
+	flag.IntVar(&gifmaxsize, "gifmax", 1048576, "max GIF size in bytes")
 	flag.StringVar(&trustedIDs, "trustedids", "", "trusted ids separated by comma")
 	flag.DurationVar(&checkpoint, "checkpoint", 60*time.Second, "checkpoint interval for state file in seconds")
 }
@@ -47,6 +54,7 @@ func main() {
 		log.Printf("Cannot log in, exiting...")
 		os.Exit(1)
 	}
+
 	botUsername = fmt.Sprintf("@%s", bot.Self.UserName)
 	log.Printf("Authorized on account %s", botUsername)
 
@@ -62,6 +70,10 @@ func main() {
 	}
 
 	log.Printf("Loaded previous state from '%s' (%d suffixes).", state, len(markov.Chain))
+
+	gifdb = NewGIFDB(gifstore)
+	gifdb.ReadList()
+	log.Printf("Loaded GIF list from '%s' (%d element/s).", gifdb.Store, len(gifdb.List))
 
 	trustedMap := make(map[int]bool)
 	if trustedIDs != "" {
@@ -89,7 +101,7 @@ func main() {
 	for update := range updates {
 		go func() {
 			answerRequired = false
-			if update.Message == nil || update.Message.Text == "" {
+			if update.Message == nil || (update.Message.Text == "" && update.Message.Document == nil) {
 				return
 			}
 
@@ -123,15 +135,36 @@ func main() {
 			}
 
 			if answerRequired {
-				genText, elapsed = markov.GenerateChain(numw, update.Message.Text)
-				log.Printf("[%s] Sending response: '%s' (generated in %s)", update.Message.From.UserName, genText, elapsed.String())
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, genText)
-				msg.ReplyToMessageID = update.Message.MessageID
-				bot.Send(msg)
+				if dice := rollDice(); dice > 4 {
+					gifpick := fmt.Sprintf("%s/%s", gifdb.Store, gifdb.GetRandom())
+					log.Printf("[%s] Sending GIF: %s", update.Message.From.UserName, gifpick)
+					msg := tgbotapi.NewDocumentUpload(update.Message.Chat.ID, gifpick)
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+				} else {
+					genText, elapsed = markov.GenerateChain(numw, update.Message.Text)
+					log.Printf("[%s] Sending response: '%s' (generated in %s)", update.Message.From.UserName, genText, elapsed.String())
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, genText)
+					msg.ReplyToMessageID = update.Message.MessageID
+					bot.Send(msg)
+				}
 			}
 
 			// add message to chain
 			markov.AddChain(strings.Join(tokens, " "))
+			if update.Message.Document != nil && (update.Message.Document.MimeType == "video/mp4" && update.Message.Document.FileSize < gifmaxsize) {
+				if err := gifdb.Hoard(update, bot); err != nil {
+					log.Printf("[%s] Could not save GIF due to error '%s'", update.Message.From.UserName, err)
+					return
+				}
+				gifdb.Add(fmt.Sprintf("%s.mp4", update.Message.Document.FileID))
+			}
 		}()
 	}
+}
+
+func rollDice() int {
+	dice := []int{1, 2, 3, 4, 5, 6}
+	rand.Seed(time.Now().UnixNano())
+	return dice[rand.Intn(len(dice)-1)]
 }
